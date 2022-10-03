47f28422b82f707984c139188625feba0c6d6666
Tago <tiagobettega@alunos.unicesumar.edu.br>
2022-10-03 16:26:39 -0300

    atualização

--- playdown.py
+++ playdown.py
@@ -1,22 +1,109 @@
-# Programa feito pelo ObvTago <3
-
-from pytube import YouTube
-from pytube import Playlist
-
+from pytube import YouTube, Playlist
+from os import system as cmd
 import os
+import random
+import tkinter as tk
+from tkinter import filedialog
+import time
 
-link = input("Insira o link de uma playlist: ")
-yt = Playlist(link)
-acre = 0;
-for url in yt.video_urls:
-    ys = YouTube(url)
-    print(ys.title)
-    vid = ys.streams.get_audio_only()
-    out_file = vid.download()
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print("Concluído")
+option_menu = {
+    1: "Baixar playlist (music)",
+    2: "Baixar musica",
+    3: "Sair",
+}
 
-print("Playlist pronta!")
-print("By ObvTago :D")
+def print_options():
+    print("------------------------------------------------------------------------")
+    print("          ###                          ###                              ")
+    print("           ##                           ##                              ")
+    print(" ######     ##      ####    ##  ##       ##    ####    ##   ##  #####   ")
+    print("  ##  ##    ##         ##   ##  ##    #####   ##  ##   ## # ##  ##  ##  ")
+    print("  ##  ##    ##      #####   ##  ##   ##  ##   ##  ##   #######  ##  ##  ")
+    print("  #####     ##     ##  ##    #####   ##  ##   ##  ##   #######  ##  ##  ")
+    print("  ##       ####     #####       ##    ######   ####     ## ##   ##  ##  ")
+    print(" ####                       #####                                       ")
+    print("------------------------------------------------------------------------")
+    print("Welcome to Playdown!")
+    print("Escolha uma opção abaixo")
+    print("------------------------------------------------------------------------")
+    for option in option_menu:
+        print(f"{option} - {option_menu[option]}")
+
+def get_option():
+    print("------------------------------------------------------------------------")
+    option = int(input(">>> "))
+    return option
+
+def main():
+    print_options()
+    option = get_option()
+    if option == 1:
+        os.system("cls" if os.name == "nt" else "clear")
+        download_playlist()
+    elif option == 2:
+        os.system("cls" if os.name == "nt" else "clear")
+        download_music()
+    elif option == 3:
+        print("Saindo...")
+        exit()
+
+def format_title(title):
+    new_title = ''
+    for ch in title:
+        if ch in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()[]{} ':
+            new_title += ch
+    return new_title
+
+def download_playlist():
+    print("--------------------------------------------------------")
+    print("Insira o URL da playlist que deseja baixar")
+    print("Exemplo: https://www.youtube.com/playlist?list=XXXXXXXXXXX")
+    print(time.strftime("%d/%m/%Y"))
+    print("--------------------------------------------------------")
+    link = input(">>> ")
+    playlist = Playlist(link)
+    acre = 0
+    x = random.randint(1, 100000)
+    print("escolha o local para salvar a playlist")
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askdirectory()
+    for url in playlist:
+        music = YouTube(url)
+        name = format_title(music.title)
+        music.streams.get_audio_only().download(f"{file_path}", filename=f"{name}" + ".mp3")
+        acre += 1
+        print(f"Baixado {acre} de {len(playlist)}")
+    print("Baixado com sucesso! \n")
+
+    if input("Deseja baixar outra playlist? (s/n) ") == "s":
+        os.system("cls" if os.name == "nt" else "clear")
+        download_playlist()
+    else:
+        main()
+
+
+def download_music():
+    print("--------------------------------------------------------")
+    print("Insira o URL da musica que deseja baixar")
+    print("Exemplo: https://www.youtube.com/watch?v=XXXXXXXXXXX")
+    print(time.strftime("%d/%m/%Y"))
+    print("--------------------------------------------------------")
+    link = input(">>> ")
+    music = YouTube(link)
+    name = format_title(music.title)
+    print("escolha o local para salvar a musica")
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askdirectory()
+    music.streams.get_audio_only().download(f"{file_path}", filename=f"{name}" + ".mp3")
+    print("Baixado com sucesso! \n")
+
+    if input("Deseja baixar outra musica? (s/n) ") == "s":
+        os.system("cls" if os.name == "nt" else "clear")
+        download_music()
+    else:
+        main()
+
+if __name__ == '__main__':
+    main()
