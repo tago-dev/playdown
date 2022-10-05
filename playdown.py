3c627281183788394019d4b8a94ed8b09bc57eb8
Flavio Tarsetti <flavio.tarsetti@idiap.ch>
2022-10-06 00:40:05 +0200

    Standardize all file with a pass of pre-commit
    Fixes #4

--- playdown.py
+++ playdown.py
@@ -1,13 +1,12 @@
-from pytube import YouTube, Playlist
-from tkinter import filedialog, Tk as tk
 import os
+from tkinter import Tk as tk
+from tkinter import filedialog
 
+from pytube import Playlist
+from pytube import YouTube
 
-option_menu = {
-    1: 'Baixar playlist',
-    2: 'Baixar música',
-    3: 'Sair'
-}
+option_menu = {1: "Baixar playlist", 2: "Baixar música", 3: "Sair"}
+
 
 def print_options():
     print()
@@ -21,16 +20,18 @@ def print_options():
     print(" ####                       #####                                       ")
     print()
     print()
-    print('Bem-vindo ao PlayDown!')
-    print('Escolha uma opção abaixo')
+    print("Bem-vindo ao PlayDown!")
+    print("Escolha uma opção abaixo")
     print()
     for option in option_menu:
-        print(f'{option}: {option_menu[option]}')
+        print(f"{option}: {option_menu[option]}")
+
 
 def get_option():
     print()
-    option = int(input('Opção: '))
+    option = int(input("Opção: "))
     return option
+
 
 def main():
     print_options()
@@ -42,27 +43,32 @@ def main():
         limpar()
         download_music()
     elif option == 3:
-        print('Saindo...')
-        os.system('exit' if os.name == 'nt' else 'exit')
+        print("Saindo...")
+        os.system("exit" if os.name == "nt" else "exit")
     else:
-        print('Número inválido!')
+        print("Número inválido!")
+
 
 def format_title(title):
-    new_title = ''
+    new_title = ""
     for ch in title:
-        if ch in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()[]{} ':
+        if (
+            ch
+            in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()[]{} "
+        ):
             new_title += ch
     return new_title
 
+
 def download_playlist():
-    print('--------------------------------------------------------')
-    print('Insira o URL da playlist que deseja baixar')
-    print('Exemplo: https://www.youtube.com/playlist?list=XXXXXXXXXXX')
-    print('--------------------------------------------------------')
-    link = input('Link: ')
+    print("--------------------------------------------------------")
+    print("Insira o URL da playlist que deseja baixar")
+    print("Exemplo: https://www.youtube.com/playlist?list=XXXXXXXXXXX")
+    print("--------------------------------------------------------")
+    link = input("Link: ")
     playlist = Playlist(link)
     acre = 0
-    print('Escolha o local para salvar a playlist')
+    print("Escolha o local para salvar a playlist")
     root = tk()
     root.withdraw()
     file_path = filedialog.askdirectory()
@@ -72,11 +78,13 @@ def download_playlist():
         author = format_title(music.author)
 
         # Baixar música e criar pasta com o nome do artista
-        music.streams.get_audio_only().download(f'{file_path}/{author}', filename=f'{name}' + '.mp3', skip_existing=True)
+        music.streams.get_audio_only().download(
+            f"{file_path}/{author}", filename=f"{name}" + ".mp3", skip_existing=True
+        )
         acre += 1
-        print(f'Baixado com sucesso! {name} - {author} \n')
+        print(f"Baixado com sucesso! {name} - {author} \n")
 
-    if input('Deseja baixar outra playlist? (s/n) ') == 's':
+    if input("Deseja baixar outra playlist? (s/n) ") == "s":
         limpar()
         download_playlist()
     else:
@@ -90,33 +98,36 @@ def new_func(audio):
 
 def download_music():
     print()
-    print('Insira o URL da musica que deseja baixar')
-    print('Exemplo: https://www.youtube.com/watch?v=XXXXXXXXXXX')
+    print("Insira o URL da musica que deseja baixar")
+    print("Exemplo: https://www.youtube.com/watch?v=XXXXXXXXXXX")
     print()
-    link = input('Link: ')
+    link = input("Link: ")
     music = YouTube(link)
     name = format_title(music.title)
-    print('Escolha o local para salvar a musica')
+    print("Escolha o local para salvar a musica")
     root = tk()
     root.withdraw()
     file_path = filedialog.askdirectory()
     author = format_title(music.author)
 
     # Baixar música e criar pasta com o nome do artista
-    music.streams.get_audio_only().download(f'{file_path}/{author}', filename=f'{name}' + '.mp3', skip_existing=True)
-    if input('Deseja baixar outra musica? (s/n) ') == 's':
+    music.streams.get_audio_only().download(
+        f"{file_path}/{author}", filename=f"{name}" + ".mp3", skip_existing=True
+    )
+    if input("Deseja baixar outra musica? (s/n) ") == "s":
         limpar()
         download_music()
     else:
         limpar()
         main()
 
+
 def limpar():
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
